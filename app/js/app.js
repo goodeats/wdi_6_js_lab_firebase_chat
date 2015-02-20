edbe140785f0c28ef41d5d9769335e602118ecad
pat <patrick.h.needham@gmail.com>
2015-02-20 17:53:35 -0500

    whatever

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+'use strict';
+
+var myDataRef = new Firebase('https://fl6kf7feo83.firebaseio-demo.com/');
+
+
+$('#messageInput').keypress(function (e) {
+  if (e.keyCode == 13) {
+    var name = $('#nameInput').val();
+    var text = $('#messageInput').val();
+    myDataRef.push({name: name, text: text});
+    $('#messageInput').val('');
+  }
+});
+
+myDataRef.on('child_added', function(snapshot) {
+  var message = snapshot.val();
+  displayChatMessage(message.name, message.text);
+});
+
+function displayChatMessage(name, text) {
+  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+};
+
+//cool
